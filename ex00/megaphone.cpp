76c3ca007c0f9212827b841c0f5cf009282b042e
toshihiro ozeki <you@example.com>
2024-08-20 22:04:48 +0900

    update: megaphone.cpp , Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
+// #include <iostream>
 
-int main(int argc, char **argv)
+
+
+// int main(int argc, char **argv)
+// {
+// 	for (int i = 0; i < argc; i++)
+// 		std::cout << argv[i] << std::endl;
+// 	return (0);
+// }
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+namespace ft
+{	
+std::string toupper(std::string str)
 {
-	for (int i = 0; i < argc; i++)
-		std::cout << argv[i] << std::endl;
-	return (0);
+	for (size_t j = 0; j < str.length(); ++j)
+	{
+		str[j] = static_cast<char>(std::toupper(str[j]));	
+	}
+	return (str);
+}
+} // namespace ft
+
+int main(int argc, char* argv[])
+{
+  for (int i = 1; i < argc; ++i)
+	std::cout << ft::toupper(argv[i]) << " ";
+  std::cout << std::endl;
+  return 0;
 }
