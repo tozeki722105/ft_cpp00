9d39be546b557182aa6e5b7d2f15e7d563872c41
toshihiro ozeki <you@example.com>
2024-08-20 22:11:52 +0900

    update: argcが1の時、エラー文を出力

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -27,8 +27,13 @@ std::string toupper(std::string str)
 
 int main(int argc, char* argv[])
 {
-  for (int i = 1; i < argc; ++i)
-	std::cout << ft::toupper(argv[i]) << " ";
-  std::cout << std::endl;
-  return 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; i < argc; ++i)
+		std::cout << ft::toupper(argv[i]) << " ";
+	std::cout << std::endl;
+	return 0;
 }
