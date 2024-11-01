bd38cac6d5ccc5ea17082b236833dc004ed92f0d
toshihiro ozeki <you@example.com>
2024-11-02 02:07:48 +0900

    clean: ex00 format

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,37 +1,25 @@
-// #include <iostream>
-
-
-
-// int main(int argc, char **argv)
-// {
-// 	for (int i = 0; i < argc; i++)
-// 		std::cout << argv[i] << std::endl;
-// 	return (0);
-// }
-
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 namespace ft
-{	
+{
 std::string toupper(std::string str)
 {
 	for (size_t i = 0; i < str.length(); i++)
 		str[i] = static_cast<char>(std::toupper(str[i]));
 	return (str);
 }
-} // namespace ft
+}  // namespace ft
 
 int main(int argc, char* argv[])
 {
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
 	for (int i = 1; i < argc; i++)
 		std::cout << ft::toupper(argv[i]);
 	std::cout << std::endl;
-	return 0;
+	return (0);
 }
